257b9866dcb3aec9e5295dc26acda02dd6e3cf18
Achim Grimm <achim.grimm@neuland-bfi.de>
2017-02-08 08:48:26 +0100

    Added fireflyContext to precondition

--- src/de/neuland/firefly/utils/GroovyScriptRunner.java
+++ src/de/neuland/firefly/utils/GroovyScriptRunner.java
@@ -5,6 +5,8 @@ import de.neuland.firefly.changes.Change;
 import de.neuland.firefly.changes.Log4JPrintStream;
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
@@ -12,9 +14,26 @@ import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.lang.Boolean.TRUE;
+import static java.util.Collections.unmodifiableMap;
+import static org.apache.commons.lang.StringUtils.*;
+
 
 @Service
 public class GroovyScriptRunner {
+    private Map<String, Boolean> fireflyContext;
+
+    @Autowired
+    public GroovyScriptRunner(@Value("${firefly.context:}") String fireflyContextConfig) {
+        Map<String, Boolean> fireflyContext = new HashMap<>();
+        for (String context : split(fireflyContextConfig, ",")) {
+            if (isNotBlank(context)) {
+                fireflyContext.put(trim(context), TRUE);
+            }
+        }
+        this.fireflyContext = unmodifiableMap(fireflyContext);
+    }
+
     public Object execute(Change change, String script) {
         ByteArrayOutputStream output = new ByteArrayOutputStream();
         Log4JPrintStream log4JPrintStream = new Log4JPrintStream(change.getChangeLogger(), new PrintStream(output));
@@ -30,6 +49,7 @@ public class GroovyScriptRunner {
         final Map<String, Object> shellContext = new HashMap<>();
         shellContext.put("ctx", Registry.getApplicationContext());
         shellContext.put("out", output);
+        shellContext.put("fireflyContext", fireflyContext);
         return shellContext;
     }
 }
